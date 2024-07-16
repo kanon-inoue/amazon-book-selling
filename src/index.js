28e88fd6f9376d0a096f4920e5f84c1d36b9374e
Kanon <n11186267@qut.edu.au>
2024-07-16 13:27:41 +1000

    Amazon Book List

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import './index.css' // one . means in the same folder
+const firstBook = {
+  author: 'J. D. Vance',
+  title: 'Hillbilly Elegy: A Memoir of a Family and Culture in Crisis',
+  img: './images/book1.png',
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const secondBook = {
+  author: 'Rebecca Yarros',
+  title: 'Onyx Storm',
+  img: './images/book2.png',
+}
+
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      >
+        <p>hdrfbiek ejrufnsierf ujnfoaeuf jYWNfhro hybfrao oayfob jhfrb</p>
+        <button>Click Me</button>
+      </Book>
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
+    </section>
+  )
+}
+
+const Book = (props) => {
+  const { img, title, author, children } = props
+  console.log(props)
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+      {children}
+    </article>
+  )
+}
+
+// const Book = ({ img, title, author }) => {
+//   return (
+//     <article className="book">
+//       <img src={img} alt={title} />
+//       <h2>{title}</h2>
+//       <h4>{author}</h4>
+//     </article>
+//   )
+// }
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<BookList />)
